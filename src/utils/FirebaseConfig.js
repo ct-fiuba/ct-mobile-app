e2b82ecd5e3c58c2bd80537b15da7da6a58e10d0
Olivia Fernandez <olifer97@gmail.com>
2020-08-16 15:01:42 -0300

    fix firebase config.

--- src/utils/FirebaseConfig.js
+++ src/utils/FirebaseConfig.js
@@ -8,12 +8,10 @@ import {
 } from 'react-native-dotenv'
 
 export const firebaseConfig = {
-    apiKey: "AIzaSyB6cuUhiQr-_94ZBbWoM9HPZ-J5yFXxVeY",
-    authDomain: "ct-fiuba.firebaseapp.com",
-    databaseURL: "https://ct-fiuba.firebaseio.com",
-    projectId: "ct-fiuba",
-    storageBucket: "ct-fiuba.appspot.com",
-    messagingSenderId: "975813637721",
-    appId: "1:975813637721:web:f808714f3dea7ac481e348",
-    measurementId: "G-Q9BHV5TPHT"
-  };
+    apiKey: API_KEY,
+    authDomain: AUTH_DOMAIN,
+    databaseURL: DATABASE_URL,
+    projectId: PROJECT_ID,
+    messagingSenderId: MESSAGE_SENDER_ID,
+    appId: APP_ID
+}
