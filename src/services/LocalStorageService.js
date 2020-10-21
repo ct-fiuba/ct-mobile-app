9af3d257a960612f02a11ee22ccf60cf57809e27
Oli <olifer97@gmail.com>
2020-10-21 16:53:24 -0300

    remove getuuid old function

--- src/services/LocalStorageService.js
+++ src/services/LocalStorageService.js
@@ -15,12 +15,6 @@ export const getAccessToken = async () => {
   return accessToken;
 };
 
-export const getuuid = () =>
-  // mejorar
-  Math.random()
-    .toString(36)
-    .substring(2) + Date.now().toString(36);
-
 export const saveScan = async scanCode => {
   const keyDate = new Date().toISOString().slice(0, 10);
   const value = {
