6bdb92fd4459b4e02be84b6c190da4f00bf43ad7
Oli <olifer97@gmail.com>
2021-06-20 15:39:16 -0300

    comments

--- src/components/RiskStatus/styles.js
+++ src/components/RiskStatus/styles.js
@@ -2,15 +2,7 @@ import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
   common: {
-    // alignItems: 'center',
-    // borderRadius: 10,
-    // elevation: 1,
-    // margin: 20,
     padding: 15,
-    // shadowOffset: { width: 1, height: 2 },
-    // shadowOpacity: 0.2,
-    // width: '100%',
-
     height: 150,
     borderBottomEndRadius: 50,
     borderBottomStartRadius: 50,
