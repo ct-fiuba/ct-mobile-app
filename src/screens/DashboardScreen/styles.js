b4efd62cca4a994a28ed76195b4c28784a626e85
Oli <olifer97@gmail.com>
2021-06-20 15:39:52 -0300

    comments

--- src/screens/DashboardScreen/styles.js
+++ src/screens/DashboardScreen/styles.js
@@ -1,16 +1,12 @@
 import { StyleSheet } from 'react-native';
 
-// import layoutStyles from '../../styles/layout';
-
 const styles = StyleSheet.create({
   actionables: {
     flexDirection: 'row',
-    // flex: 1,
     width: '100%',
     margin: 0,
   },
   center: {
-    // ...layoutStyles.center,
     margin: 10,
   },
 });
