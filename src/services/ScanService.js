4f5c8cb16adb07a11b1819d78421dd981c217b15
Tomas Accini <taccini@fb.com>
2021-08-29 21:58:42 +0100

    add logic for closing previous visits when opening new ones

--- src/services/ScanService.js
+++ src/services/ScanService.js
@@ -5,6 +5,7 @@ import { saveScan, getUserInfo, getLastVisit, clearLastVisitInfo } from './Local
 import { saveVisit, addExitTimestamp } from './CTUserAPIService';
 
 const EXIT_SCAN_VISIT_DURATION_WINDOW_MULTIPLIER = 3;
+const NEW_SCAN_CLOSING_LAST_VISIT_WINDOW_MULTIPLIER = 2;
 
 export const scan = async (scanCode, isExit, estimatedVisitDuration) => {
   if (!isExit) {
@@ -16,6 +17,11 @@ export const scan = async (scanCode, isExit, estimatedVisitDuration) => {
 const scanEntrance = async (scanCode, estimatedVisitDuration) => {
   const entranceTimestamp = new Date();
   const userInfo = await getUserInfo();
+  const lastVisit = await getLastVisit();
+  // if the we have a last visit we need to evaluate if we need to close it
+  if (lastVisit) {
+    await addExitTimestampToLastVisit(lastVisit);
+  }
   const value = {
     scanCode,
     entranceTimestamp,
@@ -34,7 +40,7 @@ const scanEntrance = async (scanCode, estimatedVisitDuration) => {
   };
   return withGenuxToken(saveVisit(value))
     .then(res => {
-      console.log(res);
+      value['estimatedVisitDuration'] = estimatedVisitDuration;
       saveScan(value);
       return res;
     })
@@ -46,6 +52,12 @@ const scanExit = async (scanCode, estimatedVisitDuration) => {
   const userInfo = await getUserInfo();
   const lastVisit = await getLastVisit();
   const closingPreviousVisit = isExitScanClosingPreviousVisit(lastVisit, scanCode, estimatedVisitDuration);
+
+  // if the we have a last visit but the current exit QR is not closing it, we need to evaluate if we need to close the previous visit
+  if (lastVisit && !closingPreviousVisit) {
+    await addExitTimestampToLastVisit(lastVisit);
+  }
+
   const value = {
     scanCode,
     exitTimestamp,
@@ -67,6 +79,7 @@ const scanExit = async (scanCode, estimatedVisitDuration) => {
       if (closingPreviousVisit) {
         clearLastVisitInfo();
       } else {
+        value['estimatedVisitDuration'] = estimatedVisitDuration;
         saveScan(value);
       }
       return res;
@@ -74,12 +87,37 @@ const scanExit = async (scanCode, estimatedVisitDuration) => {
     .catch(error => error.response);
 };
 
+const addExitTimestampToLastVisit = async (lastVisit) => {
+  if (!isPreviousVisitStillClosable(lastVisit)) {
+    clearLastVisitInfo();
+    return;
+  }
+  const exitTimestamp = new Date();
+  const value = {
+    scanCode: lastVisit.scanCode,
+    exitTimestamp,
+    userGeneratedCode: lastVisit.userGeneratedCode,
+  };
+  return withGenuxToken(addExitTimestamp(value))
+    .then(res => {
+      clearLastVisitInfo();
+      return res;
+    })
+    .catch(error => error.response);
+}
+
 const isExitScanClosingPreviousVisit = (lastVisit, scanCode, estimatedVisitDuration) => {
   if (!lastVisit || lastVisit.scanCode !== scanCode) {
     return false;
   }
   const minutesDifference = msToMinutes(new Date() - new Date(lastVisit.entranceTimestamp));
   const maximumDifference = parseInt(estimatedVisitDuration) * EXIT_SCAN_VISIT_DURATION_WINDOW_MULTIPLIER;
+  return minutesDifference <= maximumDifference;
+}
+
+const isPreviousVisitStillClosable = (lastVisit) => {
+  const minutesDifference = msToMinutes(new Date() - new Date(lastVisit.entranceTimestamp));
+  const maximumDifference = parseInt(lastVisit.estimatedVisitDuration) * NEW_SCAN_CLOSING_LAST_VISIT_WINDOW_MULTIPLIER;
   return minutesDifference <= maximumDifference;
 }
 
