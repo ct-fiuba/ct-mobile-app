3f4a21e7e680bafd2d8bd2690341c33545995f87
Oli <olifer97@gmail.com>
2021-10-23 12:55:44 -0300

    upgrade expo and fix problem with camera

--- src/screens/QRScanScreen/index.js
+++ src/screens/QRScanScreen/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useMemo } from 'react';
 import {
   View,
   Text,
@@ -6,6 +6,8 @@ import {
   SafeAreaView,
   Alert,
   TouchableOpacity,
+  Dimensions,
+  Platform,
 } from 'react-native';
 import { Camera } from 'expo-camera';
 
@@ -16,6 +18,60 @@ import styles from './styles';
 function QRScanScreen({ navigation }) {
   const [hasPermission, setHasPermission] = useState(null);
   const [scanned, setScanned] = useState(false);
+  const [ratio, setRatio] = useState('4:3'); // default is 4:3
+  const [camera, setCamera] = useState(null);
+
+  const [isRatioSet, setIsRatioSet] = useState(false);
+
+  const { height, width } = useMemo(() => Dimensions.get('window'), []);
+  const screenRatio = useMemo(() => height / width, [height, width]);
+
+  // set the camera ratio and padding.
+  // this code assumes a portrait mode screen
+  const prepareRatio = async () => {
+    let desiredRatio = '4:3'; // Start with the system default
+    // This issue only affects Android
+    if (Platform.OS === 'android') {
+      const ratios = await camera.getSupportedRatiosAsync();
+
+      // Calculate the width/height of each of the supported camera ratios
+      // These width/height are measured in landscape mode
+      // find the ratio that is closest to the screen ratio without going over
+      const distances = {};
+      const realRatios = {};
+      let minDistance = null;
+      for (const r of ratios) {
+        const parts = r.split(':');
+        const realRatio = parseInt(parts[0]) / parseInt(parts[1]);
+        realRatios[r] = realRatio;
+        // ratio can't be taller than screen, so we don't want an abs()
+        const distance = screenRatio - realRatio;
+        distances[r] = realRatio;
+        if (minDistance == null) {
+          minDistance = r;
+        } else if (distance >= 0 && distance < distances[minDistance]) {
+          minDistance = r;
+        }
+      }
+      // set the best match
+      desiredRatio = minDistance;
+      //  calculate the difference between the camera width and the screen height
+
+      // set the preview padding and preview ratio
+      setRatio(desiredRatio);
+      // Set a flag so we don't do this
+      // calculation each time the screen refreshes
+      setIsRatioSet(true);
+    }
+  };
+
+  // the camera must be loaded in order to
+  // access the supported ratios
+  const setCameraReady = async () => {
+    if (!isRatioSet) {
+      await prepareRatio();
+    }
+  };
 
   useEffect(() => {
     (async () => {
@@ -70,8 +126,13 @@ function QRScanScreen({ navigation }) {
     <SafeAreaView style={[styles.center, styles.barcodeContainer]}>
       {hasPermission ? (
         <Camera
+          onCameraReady={setCameraReady}
           onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
           style={[StyleSheet.absoluteFillObject, styles.barcode]}
+          ref={ref => {
+            setCamera(ref);
+          }}
+          ratio={ratio}
         >
           <View style={styles.layerTop}>
             <Text style={styles.title}>Escanear QR</Text>
