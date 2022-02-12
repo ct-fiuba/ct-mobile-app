a88f199daa9167cd0202447077164db5d07e2af3
Oli <olifer97@gmail.com>
2022-02-12 15:25:14 -0300

    rm googlebutton style

--- src/screens/ResetPasswordScreen/styles.js
+++ src/screens/ResetPasswordScreen/styles.js
@@ -12,16 +12,6 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     borderRadius: 22,
   },
-  googleButton: {
-    backgroundColor: '#FFFFFF',
-    height: 44,
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderRadius: 22,
-    borderWidth: 1,
-    borderColor: '#707070',
-  },
   ...layoutStyles,
   ...formStyles,
 });
